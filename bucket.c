28f4fa0e499db4cc863321dc242ab344d109d6dc
jirbthagoras <hansjabriel@gmail.com>
2026-07-08 20:35:46 +0700

    [@] sys/main/remove: added remove function, god I am a genius

--- bucket.c
+++ bucket.c
@@ -11,13 +11,12 @@ Entry *create_entry(char *key, int value) {
 };
 
 void insert(Bucket *bucket[], unsigned long hash, char *key, int value) {
-  Entry *head_entry = bucket[hash]->head;
   Entry *new_entry = create_entry(key, value);
 
-  if (head_entry == NULL)
+  if (bucket[hash]->head == NULL)
     bucket[hash]->head = new_entry;
   else {
-    Entry *current = head_entry;
+    Entry *current = bucket[hash]->head;
     while (current->next != NULL) {
       current = current->next;
     }
@@ -30,10 +29,35 @@ void *get(Bucket *bucket[], unsigned long hash, char *key) {
     return NULL;
 
   Entry *current = bucket[hash]->head;
-  while (current->next != NULL) {
+  while (current != NULL) {
     if (strcmp(key, current->key))
       return (void *)&current->value;
     current = current->next;
   }
+
   return NULL;
+}
+
+void remove(Bucket *bucket[], unsigned long hash, char *key) {
+  if (bucket[hash] == NULL)
+    return;
+
+  Entry *current = bucket[hash]->head;
+  Entry *prev;
+
+  // Loop through the entries
+  while (current != NULL) {
+    // If key matches
+    if (strcmp(key, current->key)) {
+      // If this entry is a head
+      if (current == bucket[hash]->head) {
+        bucket[hash]->head = current->next;
+      }
+      prev->next = current->next;
+      free(current);
+      return;
+    }
+    prev = current;
+    current = current->next;
+  }
 }
