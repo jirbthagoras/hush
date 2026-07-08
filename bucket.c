569c35d4676f7b43b421fd0979d323375b812beb
jirbthagoras <hansjabriel@gmail.com>
2026-07-08 20:07:02 +0700

    [@] sys/main: modified the structure

--- bucket.c
+++ bucket.c
@@ -1,6 +1,14 @@
 #include "bucket.h"
-#include "entry.h"
+#include <stdlib.h>
 #include <string.h>
+
+Entry *create_entry(char *key, int value) {
+  Entry *new_entry = malloc(sizeof(Entry));
+  new_entry->key = strdup(key);
+  new_entry->value = value;
+  new_entry->next = NULL;
+  return new_entry;
+};
 
 void insert(Bucket *bucket[], unsigned long hash, char *key, int value) {
   Entry *head_entry = bucket[hash]->head;
--- bucket.h
+++ bucket.h
@@ -1,18 +1,19 @@
 #ifndef BUCKET_H
 #define BUCKET_H
 
-#include "entry.h"
+typedef struct Entry {
+  char *key;
+  int value;
+  struct Entry *next;
+} Entry;
 
-// This is the bucket of hashed keys. This is where the hash result from djb2
-// hasher will belong. The hash reuslt also needs to be squished so that the
-// hash number is not too large and ranged between 100 - BUCKET_SIZE
 typedef struct Bucket {
   Entry *head;
 } Bucket;
 
-// Insert the key-val into bucket, first check the index, then check the Entry
-// pointed from the Bucket.
 void insert(Bucket *bucket[], unsigned long hash, char *key, int value);
 void *get(Bucket *bucket[], unsigned long hash, char *key);
+
+Entry *create_entry(char *key, int value);
 
 #endif
